3d26291f7d436cbce5f04d7c99bc83f28548a480
MD Jafor Sadek Khan <116980696+MD-Jafor-Sadek-Khan@users.noreply.github.com>
2023-09-29 22:40:14 +0600

    getting data

--- js/Ai Hub.js
+++ js/Ai Hub.js
@@ -1,1 +1,24 @@
-console.log("hello")
+const loadData = async()=>
+{
+    const res = await fetch("https://openapi.programming-hero.com/api/ai/tools")
+    const data = await res.json();
+    const models = data.data.tools;
+
+    load(models)
+}
+
+const load = async(data)=>
+{
+    data.forEach(element => {
+        const id = element.id
+        const model = fetch(`https://openapi.programming-hero.com/api/ai/tool/${id}`).then(res => res.json()).then(data => console.log(data.data))
+        
+        
+    });
+}
+
+const card = (data) =>
+{
+    const parentDiv = document.getElementById("cards-container");
+    
+}
